733293254dbfe0c075db61e3738e204ed0350e8c
schew-volpe <108027702+schew-volpe@users.noreply.github.com>
2022-10-03 16:12:57 -0400

    add scripts to github

---
+++ sc_script.R
@@ -0,0 +1,30 @@
+library(visioneval)
+source("VisionEval.R")
+# Run models ----
+
+# This works, the standard model:
+installModel("VERSPM") 
+VERSPM <- openModel('VERSPM-base')
+
+# Difficult to debut what Sharon gave us.
+# don't need to do this
+
+#installModel("VERSPM",variant = "Houston") # This only works for those that are known
+bzone_base32 <- openModel('VERSPM-base-Houston32')
+# run_model at the top (traditionally)
+# New Architecture
+# with a .cnf file, it's new 
+hgac_single <- openModel('VERSPM-HGAC_32_single')
+hgac_single$run()
+
+hgac_bike <- openModel('VERSPM-HGAC_32_more_bikes')
+hgac_bike$run()
+
+# Run models ----
+# bzone_base32 <- openModel('32 Bzone Base Model')
+# # Run models ----
+# bzone_base32 <- openModel('32 Bzone Base Model')
+hgac_single <- hgac_single$results()
+hgac_single$export()
+hgac_bike <- hgac_bike$results()
+hgac_bike$export()
